603e3b0e248e1299ed0d74cba8ce7b6cc23449f1
Darryl Jacobs <darryl@radishsystems.com>
2020-05-27 23:37:25 -0700

    update lambda for .Net core 3.1 runtime

--- functions/source/choiceview-integration/ChoiceViewAPI/Function.cs
+++ functions/source/choiceview-integration/ChoiceViewAPI/Function.cs
@@ -1,7 +1,7 @@
-
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Newtonsoft.Json.Linq;
+// ReSharper disable InconsistentNaming
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
